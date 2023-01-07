4553119c7d72302f69e349047165647d69339e82
Savio Valentino <saviovalentino57@gmail.com>
2023-01-07 10:38:12 +0700

    savio: test

--- src/main/ViewTransactionHistoryForm.java
+++ src/main/ViewTransactionHistoryForm.java
@@ -22,23 +22,29 @@ public class ViewTransactionHistoryForm extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 
-		Label selectedTransactionLbl = new Label("Selected Transaction: ");
+//		Label selectedTransactionLbl = new Label("Selected Transaction: " + );
 
 		// Codingan untuk Table View Transaction Header
-		TableView tableView1 = new TableView();
+		TableView tableView1 = new TableView<TransactionHistory>();
 		
 		TableColumn<TransactionHistory, String> column1 = new TableColumn<>("Transaction ID");
-		column1.setCellValueFactory(new PropertyValueFactory<>("transactionId"));
+		column1.setMinWidth(333);
+		column1.setCellValueFactory(new PropertyValueFactory<TransactionHistory, String>("transactionId"));
 
 		TableColumn<TransactionHistory, String> column2 = new TableColumn<>("User ID");
-		column2.setCellValueFactory(new PropertyValueFactory<>("userId"));
+		column2.setMinWidth(333);
+		column2.setCellValueFactory(new PropertyValueFactory<TransactionHistory, String>("userId"));
 
 		TableColumn<TransactionHistory, String> column3 = new TableColumn<>("Transaction Date");
-		column3.setCellValueFactory(new PropertyValueFactory<>("transactionDate"));
+		column3.setMinWidth(333);
+		column3.setCellValueFactory(new PropertyValueFactory<TransactionHistory, String>("transactionDate"));
 
 		tableView1.getColumns().add(column1);
 		tableView1.getColumns().add(column2);
 		tableView1.getColumns().add(column3);
+		
+		// Biar column nya ga bisa di resize
+		tableView1.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
 		tableView1.getItems().add(new TransactionHistory("1", "2", "2022-05-25"));
 		tableView1.getItems().add(new TransactionHistory("2", "2", "2022-05-25"));
@@ -52,6 +58,9 @@ public class ViewTransactionHistoryForm extends Application {
 		
 		// Codingan untuk Selected Transaction
 		FlowPane centerPane = new FlowPane();
+		centerPane.setPadding(new Insets(10, 0, 10, 7));
+//		centerPane.getChildren().add(selectedTransactionLbl);
+		centerPane.setAlignment(Pos.CENTER_LEFT);
 		
 		
 		// Codingan untuk Table View Transaction Detail
@@ -60,24 +69,33 @@ public class ViewTransactionHistoryForm extends Application {
 		TableView tableView2 = new TableView();
 		
 		TableColumn<TransactionHistory, String> column4 = new TableColumn<>("Transaction ID");
-		column4.setCellValueFactory(new PropertyValueFactory<>("transactionId"));
+		column4.setMinWidth(142);
+		column4.setCellValueFactory(new PropertyValueFactory<TransactionHistory, String>("transactionId"));
 		
 		TableColumn<Watch, String> column5 = new TableColumn<>("Watch ID");
-		column5.setCellValueFactory(new PropertyValueFactory<>("watchId"));
+		column5.setMinWidth(142);
+		column5.setCellValueFactory(new PropertyValueFactory<Watch, String>("watchId"));
 		
-		TableColumn<Watch, String> column6 = new TableColumn<>("Watch Name");
-		column6.setCellValueFactory(new PropertyValueFactory<>("watchName"));
+		TableColumn<Watch, String> column6 = new TableColumn<Watch, String>("Watch Name");
+		column6.setMinWidth(143);
+		column6.setCellValueFactory(new PropertyValueFactory<Watch, String>("watchName"));
 		
-		TableColumn<Watch, String> column7 = new TableColumn<>("Watch Brand");
+		TableColumn<Watch, String> column7 = new TableColumn<Watch, String>("Watch Brand");
+		column7.setMinWidth(142);
 		column7.setCellValueFactory(new PropertyValueFactory<>("watchBrand"));
 		
 		TableColumn<Watch, Integer> column8 = new TableColumn<>("Watch Price");
-		column8.setCellValueFactory(new PropertyValueFactory<>("watchPrice"));
+		column8.setMinWidth(142);
+		column8.setCellValueFactory(new PropertyValueFactory<Watch, Integer>("watchPrice"));
 		
+		// blm tau hrs ambil data dari class mana
 		TableColumn column9 = new TableColumn<>("Quantity");
+		column9.setMinWidth(142);
 		column9.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 		
-		TableColumn<ViewTransactionHistoryForm, Integer> column10 = new TableColumn<>("Sub Total");
+		// blm tau hrs ambil data dari class mana
+		TableColumn column10 = new TableColumn<>("Sub Total");
+		column10.setMinWidth(143);
 		column10.setCellValueFactory(new PropertyValueFactory<>("subTotal"));
 		
 		tableView2.getColumns().add(column4);
@@ -88,34 +106,27 @@ public class ViewTransactionHistoryForm extends Application {
 		tableView2.getColumns().add(column9);
 		tableView2.getColumns().add(column10);
 		
+		// Biar column nya ga bisa di resize
+		tableView2.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		
 		VBox vbox2 = new VBox(tableView2);
+		
 		
 		// Codingan untuk atur posisi tampilan form
 		BorderPane mainPane = new BorderPane();
 		mainPane.setTop(tableView1);
-		mainPane.setCenter(selectedTransactionLbl);
-		mainPane.getAlignment(selectedTransactionLbl);
-		mainPane.setAlignment(selectedTransactionLbl, Pos.CENTER_LEFT);
+		mainPane.setCenter(centerPane);
 		mainPane.setBottom(tableView2);
 
 		
-		
 		Scene scene = new Scene(vbox);
-		
 		Scene scene2 = new Scene(vbox2);
-		
-		Scene scene3 = new Scene(mainPane, 650, 600);
-
+		Scene scene3 = new Scene(mainPane, 999, 700);
 		primaryStage.setTitle("View Transaction History");
-
 		primaryStage.setScene(scene);
-		
 		primaryStage.setScene(scene2);
-		
 		primaryStage.setScene(scene3);
-
 		primaryStage.show();
 	}
 
 }
-
